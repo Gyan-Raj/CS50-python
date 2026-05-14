66b877769e2c14cafc4dc096370b39ce940f24f2
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-14 16:45:00 +0530

    csv.writer

---
+++ 11-input-output/08-csv package (csv.writer)/main.py
@@ -0,0 +1,11 @@
+import csv
+
+name = input("Enter the name: ")
+address = input("Enter the address: ")
+
+with open("names.csv", "a", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow([name, address])
+
+# Now, again we can have similar problem like csv.reader where we were assuming that first row will always be name and second as address
+# So, here again, we should use csv.DistWriter (09-csv package (csv.DistWriter))
