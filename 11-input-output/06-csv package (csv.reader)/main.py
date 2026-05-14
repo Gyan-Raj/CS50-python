52c6bf0cb2fdfe18603d0fc72968dee9d37ba138
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-14 16:08:20 +0530

    csv.reader

--- 11-input-output/06-csv package (csv.reader)/main.py
+++ 11-input-output/06-csv package (csv.reader)/main.py
@@ -11,3 +11,7 @@ with open("names.csv") as file:
 
 for member in sorted(members, key=lambda mem: mem["name"]):
     print(f"{member["name"]} lives in {member["address"]}")
+
+"""
+We can mention in csv file at top the column names that we want and use csv.DictReader which will give us the names at top (07-csv package (csv.DictReader))
+"""
