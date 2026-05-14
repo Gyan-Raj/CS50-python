61fc5e0dfd17deadd528bf8673d20afba0dd0863
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-14 11:02:28 +0530

    handled the AssertionError in except block

--- 10-unit testing/03-assert-in-try-except/test_calculator.py
+++ 10-unit testing/03-assert-in-try-except/test_calculator.py
@@ -8,11 +8,11 @@ def main():
 def test_square():
     try:
         assert square(2) == 4
-    except:
+    except AssertionError:
         print("2 squared is not 4")
     try:
         assert square(3) == 9
-    except:
+    except AssertionError:
         print("3 squared is not 9")
 
 
