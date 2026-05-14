94359fb735c29992e1f4c8711de0f2e57af4f973
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-14 12:50:40 +0530

    need to close the file system after writing content

--- 11-input-output/01-write (open)/main.py
+++ 11-input-output/01-write (open)/main.py
@@ -1,7 +1,10 @@
 name = input("Enter your name: ")
 
 # Everytime we run "python main.py" and give a name, it will override the file (It will not append)
-open("names.txt", "w").write(name)
+
+file = open("names.txt", "w")
+file.write(name)
+file.close()  # We need to open and then finally close it
 
 # names.txt is the file which will be created
 # "w" is for write
