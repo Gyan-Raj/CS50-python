8c7b0907def402870c14eefb683f23dc97dd9831
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-18 13:55:07 +0530

    raising exceptions in classes

---
+++ 13-OOPs/02/01_raise_exceptions_in_classes.py
@@ -0,0 +1,25 @@
+class User:
+    def __init__(self, first_name, last_name):
+        if not first_name:
+            raise ValueError("first_name cannot be empty")
+        if last_name not in ["Raj", "raj"]:
+            raise ValueError("Last name is incorrect")
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+def main():
+    user = get_user()
+    if user.first_name.lower() == "gyan":
+        user.last_name = "Raj"
+    print(f"Hi, {user.first_name} {user.last_name}")
+
+
+def get_user():
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    return User(first_name, last_name)
+
+
+if __name__ == "__main__":
+    main()
