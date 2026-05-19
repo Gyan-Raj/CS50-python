4de4b61ff6472774d8476c872f93112d69faec03
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-19 16:52:44 +0530

    introductiont to generators (yield)

---
+++ 14-et cetera/17_generators.py
@@ -0,0 +1,19 @@
+def main():
+    n = int(input("Enter a number: "))
+    for s in sheep(n):
+        print(s)
+
+
+def sheep(n):
+    flock = []
+    for i in range(n):
+        flock.append("🐑" * i)
+    return flock
+
+
+if __name__ == "__main__":
+    main()
+
+# This code above works perfect for small numbers like 1 or 10 or 100 or 1000 or 10000 or even for 100000. But for higher numbers, the app crashes
+# This is because the system is trying to print all at once.
+# To overcome this, we can use genrators (yield) (18_generators.py)
