fdfa723a41f25427494d496f41759abc3ac4bcb1
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-19 15:06:58 +0530

    using ** for unpacking dictionaries

--- 14-et cetera/09_unpacking.py
+++ 14-et cetera/09_unpacking.py
@@ -6,4 +6,17 @@ def total(phy, chem, maths):
 
 
 marks = [80, 83, 93]
-print(f"Total marks: {total(*marks)}")
+print(f"Total marks using indexing: {total(marks[0], marks[1], marks[2])}")
+print(f"Total marks using unpacking: {total(*marks)}")
+
+
+# To unpack dictionaries, we use "**"
+marks_dict = {
+    "phy": 80,
+    "chem": 83,
+    "maths": 93
+}
+print(
+    f"Total marks using dictionaries: {total(marks_dict["phy"], marks_dict["chem"], marks_dict["maths"])}")
+print(
+    f"Total marks using unpacking dictionaries: {total(**marks_dict)}")
