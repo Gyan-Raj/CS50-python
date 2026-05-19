77a89e87652d4a8ddeba1c63682b83d1db85937f
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-19 12:13:27 +0530

    return type of the funciton

--- 14-et cetera/06_type_hints/01_type_hints.py
+++ 14-et cetera/06_type_hints/01_type_hints.py
@@ -2,7 +2,7 @@
 # We can however use a tool "typing" provided in "mypy" package (pip install mypy) which can do type checking for us. This will not break the code but will just highlight the type mismatches before releasing the code
 # Run this file using "mypy 01_type_hints.py" beofre running "python 01_type_hints.py"
 
-def meow(n: int):
+def meow(n: int) -> None:
     for _ in range(n):
         print("meow")
 
@@ -15,4 +15,3 @@ When we run "mypy 01_type_hints.py", it will show this output:
 C:\Users\bhard\Desktop\learning\CS50-python\14-et cetera\06_type_hints> mypy .\01_type_hints.py
 Success: no issues found in 1 source file
 """
-pip freeze > requirements.txt
